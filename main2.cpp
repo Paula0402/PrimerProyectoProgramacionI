77d0dc79c9183b21259009bd5821435679de2fa6
Paula0402 <paula.jimenacr05@gmail.com>
2024-10-18 23:59:42 -0600

    Team 2 Primer Proyecto Programacion I

--- main2.cpp
+++ main2.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-
+    
     int Factura = 0, CantidadTiquetes, TipoCliente, TipoTiquete;
     char Cedula[20], Nombre[50];
     char Continuar = 'S';
@@ -9,43 +9,52 @@ int main() {
     int TotalGaleria = 0, TotalPalco = 0, TotalEntradas = 0;
     double AcumuladoGaleria = 0, AcumuladoPalco = 0, AcumuladoGeneral = 0;
     
-
     while (Continuar == 'S' || Continuar == 's') {
-        
-        Factura = Factura + 1;
-
        
-        printf("\nFactura No: ");
-        scanf("%d", &Factura);
+        Factura++;
+
+     
+        printf("\nFactura No: %d\n", Factura);
         printf("Ingrese la cédula del cliente: ");
         scanf("%s", Cedula);
         printf("Ingrese el nombre del cliente: ");
         scanf("%s", Nombre);
 
-       
+     
         do {
             printf("Ingrese el tipo de cliente (1=Niño/Adulto mayor, 2=Adulto): ");
             scanf("%d", &TipoCliente);
+            if (TipoCliente != 1 && TipoCliente != 2) {
+                printf("Error: Ingrese un tipo de cliente válido (1 o 2).\n");
+            }
         } while (TipoCliente != 1 && TipoCliente != 2);
 
-        
+     
         do {
             printf("Ingrese el tipo de tiquete (1=Galería, 2=Palco): ");
             scanf("%d", &TipoTiquete);
+            if (TipoTiquete != 1 && TipoTiquete != 2) {
+                printf("Error: Ingrese un tipo de tiquete válido (1 o 2).\n");
+            }
         } while (TipoTiquete != 1 && TipoTiquete != 2);
 
-        
-        printf("Ingrese la cantidad de tiquetes o entradas a comprar: ");
-        scanf("%d", &CantidadTiquetes);
+    
+        do {
+            printf("Ingrese la cantidad de tiquetes o entradas a comprar: ");
+            scanf("%d", &CantidadTiquetes);
+            if (CantidadTiquetes <= 0) {
+                printf("Error: La cantidad de tiquetes debe ser mayor a 0.\n");
+            }
+        } while (CantidadTiquetes <= 0);
 
-        
+
         if (TipoTiquete == 1) {
             MontoTiquete = 6000; 
         } else if (TipoTiquete == 2) {
             MontoTiquete = 12000; 
         }
 
-        
+
         MontoVenta = MontoTiquete * CantidadTiquetes;
 
         
@@ -57,28 +66,27 @@ int main() {
 
         
         Subtotal = MontoVenta - Descuento;
-        Impuesto = Subtotal * 0.13; 
+        Impuesto = Subtotal * 0.13;
         Total = Subtotal + Impuesto;
 
-
+        
         printf("\nDetalle de la Venta\n");
         printf("Factura No: %d\n", Factura);
         printf("Cliente: %s, Cédula: %s\n", Nombre, Cedula);
 
-  
+        
         if (TipoCliente == 1) {
             printf("Tipo de Cliente: Niño/Adulto Mayor\n");
         } else {
             printf("Tipo de Cliente: Adulto\n");
         }
 
-
+        
         if (TipoTiquete == 1) {
             printf("Tipo de Tiquete: Galería\n");
         } else {
             printf("Tipo de Tiquete: Palco\n");
         }
-
 
         printf("Cantidad de tiquetes: %d\n", CantidadTiquetes);
         printf("Monto unitario por tiquete: %.2f\n", MontoTiquete);
@@ -88,25 +96,22 @@ int main() {
         printf("Impuesto de venta: %.2f\n", Impuesto);
         printf("Total a pagar: %.2f\n", Total);
 
-
+        
         if (TipoTiquete == 1) {
-            
-			AcumuladoGaleria = AcumuladoGaleria + Total;
-			TotalGaleria = TotalGaleria + CantidadTiquetes;
-            
+            AcumuladoGaleria += Total;
+            TotalGaleria += CantidadTiquetes;
         } else if (TipoTiquete == 2) {
-        	
-            AcumuladoPalco = AcumuladoPalco + Total;
-			TotalPalco = TotalPalco + CantidadTiquetes;
+            AcumuladoPalco += Total;
+            TotalPalco += CantidadTiquetes;
         }
         
-        AcumuladoGeneral = AcumuladoGeneral + Total;
-        TotalEntradas = TotalEntradas + CantidadTiquetes;
+        AcumuladoGeneral += Total;
+        TotalEntradas += CantidadTiquetes;
 
+        
         printf("\n¿Desea registrar otra venta? (S/N): ");
         scanf(" %c", &Continuar);
     }
-
 
     printf("\nEstadísticas\n");
     printf("Cantidad de entradas tiquetes galería: %d\n", TotalGaleria);
@@ -115,6 +120,7 @@ int main() {
     printf("Acumulado dinero por tiquetes palco: %.2f\n", AcumuladoPalco);
     printf("Cantidad general de entradas: %d\n", TotalEntradas);
     printf("Acumulado general dinero por entradas: %.2f\n", AcumuladoGeneral);
+    
     if (Factura > 0) {
         printf("Promedio general por venta: %.2f\n", AcumuladoGeneral / Factura);
     } else {
--- main3.cpp
+++ main3.cpp
@@ -2,34 +2,31 @@
 #include <stdio.h>
 
 int main() {
-    int Numero, NumeroInverso, Digito;
+    int Numero, NumeroInverso, Digito, NumeroOriginal;  
     char Continuar;
 
     do {
-        
-        printf("Digite un numero entero de máximo cinco dígitos: ");
+        printf("Digite un número entero de máximo cinco dígitos: ");
         scanf("%i", &Numero);
 
-        
         if (Numero < 1 || Numero > 99999) {
             printf("El número debe ser menor a 99999.\n");
-            
         } else {
-        	
-            NumeroInverso = 0;  
+            NumeroOriginal = Numero;  
+            NumeroInverso = 0;
 
             while (Numero > 0) {
-                Digito = Numero % 10;     
-				Numero = Numero / 10;             
-                NumeroInverso = NumeroInverso * 10 + Digito; 
-                
+                Digito = Numero % 10;
+                Numero = Numero / 10;
+                NumeroInverso = NumeroInverso * 10 + Digito;
             }
 
+            printf("El número ingresado es: %i\n", NumeroOriginal); 
             printf("El número inverso es: %d\n", NumeroInverso);
         }
 
         printf("¿Desea digitar otro número? (s/n): ");
-        scanf(" %c", &Continuar); 
+        scanf(" %c", &Continuar);
 
     } while (Continuar == 's' || Continuar == 'S');
 
